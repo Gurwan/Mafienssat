218d34e31347b27c018143b039c31eba958cebc5
TarKhnarb <TarKhnarb>
2022-02-03 12:47:23 +0100

    /: db.sqlite3: new database according models.py /main_app/: 0001_initial.py: new migration according models.py models.py: correction of primary keys and correct redefinition of user.sector and storebet.result

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -9,17 +11,18 @@ class Klaxcoin(models.Model):
 
 # User
 class User(AbstractUser):
-    id_user = models.AutoField(primary_key=True, unique=True)
+    id = models.AutoField(primary_key=True, unique=True)
     firstname = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     username = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
-    sector = (
+    SECTOR = (
         ('INFO', 'Informatique'),
         ('PHOT', 'Photonique'),
         ('SNUM', 'Systèmes numériques'),
         ('IMR', 'IMR')
     )
+    sector = models.CharField(max_length=4, choices=SECTOR, blank=True, help_text='Donne moi ta filière bro')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     klax_coins = models.OneToOneField(
@@ -30,7 +33,7 @@ class User(AbstractUser):
 
 
 class Bets(models.Model):
-    id_bet = models.AutoField(primary_key=True, unique=True)
+    id = models.AutoField(primary_key=True, unique=True)
     bet_name = models.CharField(max_length=256, unique=True)
 
     win_rate = models.DecimalField(max_digits=4, decimal_places=2)
@@ -52,11 +55,12 @@ class Bets(models.Model):
 class StoreBets(models.Model):
     bet_id = models.ForeignKey('Bets', on_delete=models.PROTECT)
     user_id = models.ForeignKey('User', on_delete=models.PROTECT)
-    choice = (
+    RESULT = (
         ('W', 'Win'),
         ('D', 'Draw'),
         ('L', 'Lose')
     )
+    result = models.CharField(max_length=1, choices=RESULT, blank=True, help_text='Which result for the bet')
     gains = models.DecimalField(max_digits=12, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     is_combined = models.BooleanField(default=False)
