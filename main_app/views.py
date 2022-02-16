0d70e7dc677cd6fef16c632941c7a1298548bda6
TarKhnarb <TarKhnarb>
2022-02-16 19:05:01 +0100

    urls.py: ajout de la fonction ratingRecalculation, addGains et finalizeBet myBetKlax/betKlax.html: modification des fonctionnalités de la page en cours klaxment.html: rectification pour classer les utilisateurs dans le bon ordre views.py: ajout des fonctions et modification des fonctionnalités ajoutées

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.db.models import Q
 from decimal import *
 from .models import User, Bets, StoreBets
 from .forms import UserForm, AddBetForm
@@ -73,25 +72,47 @@ def addBet(request):
         messages.error(request, "Tu dois être membre du staff pour accéder à cette page")
 
 
+def ratingRecalculation(request):
+    bet_id = request.POST['bet']
+    bet = Bets.objects.all().get(pk=bet_id)
+    if bet is not None:
+        w_gains = float(bet.win_gains) if bet.win_gains > Decimal(0) else Decimal(1)
+        l_gains = float(bet.lose_gains) if bet.lose_gains > Decimal(0) else Decimal(1)
+        w_rate = (w_gains + l_gains)/w_gains
+        l_rate = (w_gains + l_gains)/l_gains
+        bet.win_rate = Decimal(w_rate)
+        bet.lose_rate = Decimal(l_rate)
+        bet.save()
+
+        name = request.POST['name']
+        if name is not None:
+            return redirect(name)
+        else:
+            return redirect('home')
+    else:
+        messages.error(request, "Problème rencontré lors du recalcul des cotes")
+
+
 def makeBetW(request):
     if request.user is not None:
         current_user = User.objects.get(pk=request.user.id)
         primary_key = request.POST['bet'] if request.POST['bet'] is not None else messages.error(request,
                                                                                                  "Votre demande ne peut aboutir actuellement, si cella perciste, veuollez contacter un admin du site")
-        gains = request.POST['gains'] if request.POST['gains'] is not None else messages.error(request,
-                                                                                               "Votre demande ne peut aboutir actuellement, si cella perciste, veuollez contacter un admin du site")
+
         cdt = StoreBets.objects.filter(bet_id_id=primary_key).filter(user_id_id=current_user)
         if cdt.count() == 0:
-            if current_user.klax_coins >= Decimal(gains):
-                current_user.klax_coins -= Decimal(gains)
+            if current_user.klax_coins > 0:
+
+                current_user.klax_coins -= 1
                 current_user.save()
 
                 myBet = StoreBets()
-                myBet.bet_name = Bets.objects.get(id=primary_key).bet_name
                 myBet.result = 'W'
-                myBet.gains = gains
+                myBet.gains = 1
                 myBet.bet_id = Bets.objects.get(id=primary_key)
                 myBet.user_id = request.user
+                myBet.bet_id.win_gains += 1
+                myBet.bet_id.win_vote += 1
                 myBet.save()
             else:
                 messages.error(request, "Tu n\'as pas asser de klax_coins espèce de rat")
@@ -108,26 +129,24 @@ def makeBetL(request):
     if current_user is not None:
         primary_key = request.POST['bet'] if request.POST['bet'] is not None else messages.error(request,
                                                                                                  "Votre demande ne peut aboutir actuellement, si cella perciste, veuollez contacter un admin du site")
-        gains = request.POST['gains'] if request.POST['gains'] is not None else messages.error(request,
-                                                                                               "Votre demande ne peut aboutir actuellement, si cella perciste, veuollez contacter un admin du site")
         cdt = StoreBets.objects.filter(bet_id_id=primary_key).filter(user_id_id=current_user)
         if cdt.count() == 0:
-            if current_user.klax_coins >= Decimal(gains):
-                current_user.klax_coins -= Decimal(gains)
+            if current_user.klax_coins > 0:
+                current_user.klax_coins -= 1
                 current_user.save()
 
                 myBet = StoreBets()
-                myBet.bet_name = Bets.objects.get(id=primary_key).bet_name
                 myBet.result = 'L'
-                myBet.gains = gains
+                myBet.gains = 1
                 myBet.bet_id = Bets.objects.get(id=primary_key)
                 myBet.user_id = request.user
+                myBet.bet_id.lose_gains += 1
+                myBet.bet_id.lose_vote += 1
                 myBet.save()
             else:
                 messages.error(request, "Tu n\'as pas asser de klax_coins espèce de rat")
         else:
             messages.error(request, "Tu as déja parié sur ce pari")
-
     else:
         messages.error(request, "Il faut te connecter pour parier")
 
@@ -137,11 +156,74 @@ def makeBetL(request):
 def myBets(request):
     current_user = request.user
     if current_user is not None:
-        mybets = StoreBets.objects.all().filter(user_id_id=current_user.id)
+        finalizedBets = StoreBets.objects.all().filter(user_id_id=current_user.id, blocked_bet=True)
+        finalId = []
+        for b in finalizedBets:
+            finalId.append(b.bet_id.id)
+        mybets = StoreBets.objects.all().filter(user_id_id=current_user.id).exclude(bet_id_id__in=finalId)
         user = User.objects.get(pk=current_user.id)
-        return render(request, 'myBetKlax.html', {'mybets': mybets, 'user': user})
+        # todo ajouter la requête permettant de récupérer les combinés dans tab 2D
+
+        return render(request, 'myBetKlax.html', {'mybets': mybets, 'finalizedBets': finalizedBets, 'user': user})
     else:
         messages.error(request, "Connecte toi")
+
+
+def addGains(request):
+    current_user = User.objects.get(pk=request.user.id)
+    if current_user is not None:
+        bet_id = request.POST['bet']
+        gains = request.POST['gains']
+        print(current_user.id)
+        if current_user.klax_coins >= Decimal(gains):
+            print(bet_id)
+            bet = StoreBets.objects.all().get(user_id_id=current_user.id, bet_id_id=bet_id)
+            bet.gains += Decimal(gains)
+            if bet.result == 'W':
+                bet.bet_id.win_gains += Decimal(gains)
+            else:
+                bet.bet_id.lose_gains += Decimal(gains)
+            bet.save()
+
+            current_user.klax_coins -= Decimal(gains)
+            current_user.save()
+        else:
+            messages.error(request, "Tu n'as pas asser de KlaxCoins espèce de rat")
+    else:
+        messages.error(request, "Il faut être connecté pour acceder à cette page")
+    return redirect("myBets")
+
+
+def finalizeBet(request):
+    current_user = User.objects.get(pk=request.user.id)
+    if current_user is not None:
+        bet_id = request.POST['bet']
+        gains = request.POST['gains']
+        if current_user.klax_coins >= Decimal(gains):
+            if bet_id is not None:
+                bet = StoreBets.objects.get(bet_id_id=bet_id, user_id_id=current_user.id)
+                if bet is not None:
+                    current_user.klax_coins -= Decimal(gains)
+                    current_user.save()
+
+                    bet.is_combined = False
+                    bet.id_combined = bet_id
+                    bet.gains += Decimal(gains)
+                    if bet.result == 'W':
+                        bet.bet_id.win_gains += Decimal(gains)
+                        bet.bet_rate = bet.bet_id.win_rate
+                    else:
+                        bet.bet_id.lose_gains += Decimal(gains)
+                        bet.bet_rate = bet.bet_id.lose_rate
+                    bet.save()
+            else:
+                messages.error(request, "Contacte les admins si le problème perciste après un refresh de la page")
+        else:
+            messages.error(request, "Tu n'as pas assez de KlaxCoins espèce de rat")
+    else:
+        messages.error(request, "Tu dois être connecté pour accéder à cette page")
+
+    return redirect("myBets")
 
 
 def betKlax(request):
@@ -168,7 +250,8 @@ def event(request):
 def liste(request):
     return render(request, 'liste.html')
 
+
 def klaxment(request):
-    userList = User.objects.all()  # va chercher tous les utilisateurs du site
+    userList = User.objects.all().order_by('-klax_coins')  # va chercher tous les utilisateurs du site
     data = {'userList': userList}
-    return render(request, 'klaxment.html',data)
+    return render(request, 'klaxment.html', data)
