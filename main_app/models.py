96d6aa30f4859d97dd67a9acfa281f578faf1186
TarKhnarb <TarKhnarb>
2022-02-17 20:26:09 +0100

    Ajout du texte d'aide pour entrer les dates Suppression de 'Draw' dans StoreBets Ajout de nouveaux types d'évènement Ajout de la colonne associated_bet pour Event Ajout de la clé primaire de Registration

--- main_app/models.py
+++ main_app/models.py
@@ -36,7 +36,7 @@ class Bets(models.Model):
     lose_gains = models.DecimalField(max_digits=12, decimal_places=2, default=1.00)
 
     created = models.DateTimeField(auto_now_add=True)
-    ended = models.DateTimeField()
+    ended = models.DateTimeField(help_text='YYYY-MM-DD HH:MM:SS')
 
 
 class StoreBets(models.Model):
@@ -44,7 +44,6 @@ class StoreBets(models.Model):
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     RESULT = (
         ('W', 'Win'),
-        ('D', 'Draw'),
         ('L', 'Lose')
     )
     result = models.CharField(max_length=1, choices=RESULT, blank=True, help_text='Which result for the bet')
@@ -60,18 +59,24 @@ class Event(models.Model):
     event_description = models.CharField(max_length=1024)
     TYPE = (
         ('A', 'Annonce'),
-        ('S', 'Soirée'),
+        ('C', 'Chasse au trésor'),
+        ('E', 'E-Sport'),
         ('J', 'Jeux'),
-        ('S', 'Sport'),
-        ('E', 'E-Sport') #Concours,
+        ('P', 'Prévention'),
+        ('K', 'Ker\'mess'),
+        ('So', 'Soirée'),
+        ('Sp', 'Sport'),
+        ('T', 'Tournoi')
     )
-    event_type = models.CharField(max_length=1, choices=TYPE, blank=True, help_text='Which type of event')
-    event_date = models.DateTimeField()
+    event_type = models.CharField(max_length=2, choices=TYPE, blank=True, help_text='Which type of event')
+    event_date = models.DateTimeField(help_text='YYYY-MM-DD HH:MM:SS')
     attendees_number = models.IntegerField(default=0)
     max_attendees = models.IntegerField(null=True)
+    associated_bet = models.BooleanField(default=False)
 
 
 class Registrations(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
     event_id = models.ForeignKey('Event', on_delete=models.CASCADE)
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
 
