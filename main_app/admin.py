3cc2867f9de877b3a8b27bb142d5257cd857e1e7
TarKhnarb <TarKhnarb>
2022-02-25 12:11:56 +0100

    Start changes on allos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Bets, StoreBets, Event, EventRegistration, Allos, AllosRegistration
+from .models import User, Bets, StoreBets, Event, EventRegistration, Allos, AllosRegistration, AllosUserCounters
 
 admin.site.register(User)
 admin.site.register(Bets)
@@ -10,3 +10,4 @@ admin.site.register(Event)
 admin.site.register(EventRegistration)
 admin.site.register(Allos)
 admin.site.register(AllosRegistration)
+admin.site.register(AllosUserCounters)
