01e2880ee788f67634a781e921d7e4606913dbd8
TarKhnarb <TarKhnarb>
2022-02-26 18:44:19 +0100

    Adding labels on fields forms

--- main_app/forms.py
+++ main_app/forms.py
@@ -8,12 +8,12 @@ class UserForm(UserCreationForm):
         model = User
         fields = ['firstname', 'lastname', 'username', 'email', 'sector', 'password1', 'password2']
         labels = {
-            'firstname' : 'Prénom',
-            'lastname' : 'Nom',
-            'username' : 'Pseudo',
-            'sector' : 'Filière',
-            'password1' : 'Mot de passe',
-            'password2' : 'Confirmer le mot de passe ',
+            'firstname': 'Prénom',
+            'lastname': 'Nom',
+            'username': 'Pseudo',
+            'sector': 'Filière',
+            'password1': 'Mot de passe',
+            'password2': 'Confirmer le mot de passe ',
         }
 
 
@@ -21,18 +21,37 @@ class AddBetForm(ModelForm):
     class Meta:
         model = Bets
         fields = ['bet_name', 'ended']
+        labels = {
+            'bet_name': 'Nom du pari',
+            'ended': 'Date et heure de fin pour parier au format YYYY-MM-DD HH:MM:SS'
+        }
 
 
 class AddEventForm(ModelForm):
     class Meta:
         model = Event
         fields = ['event_name', 'event_type', 'event_description', 'event_date', 'max_attendees', 'associated_bet']
+        labels = {
+            'event_name': 'Nom de l\'event',
+            'event_type': 'Type de l\'event',
+            'event_description': 'Description succincte de l\'event',
+            'event_date': 'Date et heure de l\'event au format YYYY-MM-DD HH:MM:SS',
+            'max_attendees' : 'Nombre maximum de participant 0 si pas de jauge',
+            'associated_bet': 'À cocher si il y a un pari associé'
+        }
 
 
 class AlloAdminForm(ModelForm):
     class Meta:
         model = Allos
         fields = ['allo_type', 'description', 'cost', 'start_date', 'end_date']
+        labels = {
+            'allo_type': 'Type de allo à proposer',
+            'description': 'Description succincte du allo',
+            'cost': 'Coût en klaxcoins du allo',
+            'start_date': 'Date et heure de début de disponibilité au format YYYY-MM-DD HH:MM:SS',
+            'end_date': 'Date et heure de fin de disponibilité au format YYYY-MM-DD HH:MM:SS'
+        }
 
 
 class AlloForm(ModelForm):
