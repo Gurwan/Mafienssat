c510d7e4abd2bcf05192a6b5007ceb545e9e24b4
TarKhnarb <TarKhnarb>
2022-03-15 22:27:29 +0100

    Corrections

--- bet_klax/settings.py
+++ bet_klax/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.template.context_processors import static
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -118,7 +120,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), BASE_DIR.joinpath('static')]
 
