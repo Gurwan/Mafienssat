853032adc6158882d442a45e4fd2e8d2c6102fa8
root <root@s22958634.onlinehome-server.info>
2022-03-15 07:25:43 +0100

    setting KEY

--- bet_klax/settings.py
+++ bet_klax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4z#**v4$(7icr7v^um0$q&)yr5*#$q7-hfvr1q=lw_swtlzjt^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [] #['s22958634.onlinehome-server.info']
+ALLOWED_HOSTS = ['s22958634.onlinehome-server.info']
 
 # Application definition
 
