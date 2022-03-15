aea675b98a27d20ab2da8b843b6b67c445965c1c
root <root@s22958634.onlinehome-server.info>
2022-03-15 11:50:12 +0100

    ok

--- bet_klax/settings.py
+++ bet_klax/settings.py
@@ -120,7 +120,7 @@ USE_TZ = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), 'var/www/bet_klax/static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), 'var/www/bet_klax/static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
