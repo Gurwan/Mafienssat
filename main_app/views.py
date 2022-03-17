1a5c84dc030917b2f00f85dca92c20ea92bef741
TarKhnarb <TarKhnarb>
2022-03-17 02:00:50 +0100

    Corrections

--- main_app/views.py
+++ main_app/views.py
@@ -555,7 +555,7 @@ def eventHTML(request, id_event):
 
     if this_event is not None:
 
-        infos = readFileForHTML(os.path.join(settings.BASE_DIR, 'static/') + 'events/' + this_event.event_name + '.txt')
+        infos = readFileForHTML('/var/www/bet_klax/static/events/' + this_event.event_name + '.txt')
 
         return render(request, 'events/eventPresentation.html', {'event': this_event, 'infos': infos})
     else:
