89fc0fe18b2ab1d9b0b9bedaab49746a06e3265d
TarKhnarb <TarKhnarb>
2022-03-20 14:59:20 +0100

    Corrections

--- main_app/views.py
+++ main_app/views.py
@@ -966,9 +966,9 @@ def alloRequested(request):
             if tmp is not None:
                 all_done.append(tmp)
 
-        allo_nb = all_requested.count()
-        take_nb = all_take.count()
-        done_nb = all_done.count()
+        allo_nb = all_requested.count
+        take_nb = all_take.count
+        done_nb = all_done.count
         return render(request, 'allos/alloRequested.html',
                       {'user': user, 'allos': all_requested, 'alloNb': allo_nb, 'takeOver': all_take, 'takeNb': take_nb, 'doneAllos': all_done, 'doneNb': done_nb})
     else:
